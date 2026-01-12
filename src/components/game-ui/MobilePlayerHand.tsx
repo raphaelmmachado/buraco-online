ba0f9c5800ec8d54159e42e7eff4cc9eb064a918
Raphael Machado <raphael.mm.91@gmail.com>
2026-01-12 02:14:08 -0300

    deploy render

--- src/components/game-ui/MobilePlayerHand.tsx
+++ src/components/game-ui/MobilePlayerHand.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { type Card as CardType } from "../../../common/types/card";
 import { HandCard } from "./HandCard";
 

6bd285d5e026b0a2167c5af3a8a945a422a1a416
Raphael Machado <raphael.mm.91@gmail.com>
2026-01-16 02:11:48 -0300

    tentativas de resolver quedas de conexão

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,8 @@ import { registerGameHandlers } from "./controllers/gameController";
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const httpServer = createServer();
 const io = new Server(httpServer, {
+  pingInterval: 25000, // 25 seconds heartbeat
+  pingTimeout: 20000,  // 20 seconds tolerance
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
